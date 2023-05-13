557a0d4c53ceed319f204f443b6209aee5b05db3
imjayapple <imjayapple@gmail.com>
2023-05-12 21:23:34 -0700

    Define response based on user request (city)

--- weather-report.py
+++ weather-report.py
@@ -3,3 +3,12 @@ import requests
 def get_weather(city_name):
     api_key = "your_openweathermap_api_key"
     base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    # Use the 'params' parameter to add the API key and city name to the request
+
+    response = requests.get(base_url, params={
+        'q': city_name,
+        'appid': api_key
+    })
+
+    data = response.json()
