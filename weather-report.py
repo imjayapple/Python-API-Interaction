4c63de8049810cab586e14b9960d091e8ebe3fb6
imjayapple <imjayapple@gmail.com>
2023-05-16 00:01:49 -0700

    Added prompt to use own key

--- weather-report.py
+++ weather-report.py
@@ -1,6 +1,7 @@
 import requests
 
 def get_weather(city_name):
+    # Use your own key here
     api_key = "your_openweathermap_api_key"
     base_url = "http://api.openweathermap.org/data/2.5/weather"
 
