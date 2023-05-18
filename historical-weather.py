7d701f7e0434dbec160e01a66a027020b1096b1f
imjayapple <imjayapple@gmail.com>
2023-05-17 23:50:35 -0700

    Historial weather python app

---
+++ historical-weather.py
@@ -0,0 +1,24 @@
+import requests
+
+# Set up the API endpoint and parameters
+api_key = "your_openweathermap_api_key"
+endpoint = "https://api.openweathermap.org/data/2.5/onecall/timemachine"
+params = {
+    "lat": 40.712776,
+    "lon": -74.005974,
+    "dt": 1621228800, # Unix timestamp for May 17, 2021
+    "appid": api_key,
+    "units": "metric"
+}
+
+# Make the API request
+response = requests.get(endpoint, params=params)
+
+# Parse the response data as JSON
+data = response.json()
+
+# Extract the temperature and weather description from the data
+temperature = data["current"]["temp"]
+weather_desc = data["current"]["weather"][0]["description"]
+
+print(f"The temperature in New York City on May 17, 2021 was {temperature} degrees Celsius, and the weather was {weather_desc}.")
